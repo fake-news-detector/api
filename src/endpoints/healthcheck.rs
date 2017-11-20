d886742cbc08deff02a3a70f1f8dd622f212366c
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-11-20 16:01:18 -0200

    Add unitTest for healthcheck

--- src/endpoints/healthcheck.rs
+++ src/endpoints/healthcheck.rs
@@ -7,3 +7,16 @@ use rocket::http::Status;
 fn healthcheck() -> status::Custom<String> {
     status::Custom(Status::Ok, String::from("OK"))
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn return_custom_status_ok() {
+        let health_status = healthcheck();
+        let expected_status = status::Custom(Status::Ok, String::from("OK"));
+        assert_eq!(health_status, expected_status);
+    }
+}
